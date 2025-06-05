2c798834e965d9f441717c9ecb17a0762fe57473
Sia <siavash_p@hotmail.com>
2025-06-05 11:29:24 -0700

    Changed title

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 export function Header() {
     const title = document.createElement('h1');
     title.className = 'page-title';
-    title.textContent = 'Welcome to My Profile...';
+    title.textContent = 'Welcome to My Portfolio...';
 
     return title;
 }
